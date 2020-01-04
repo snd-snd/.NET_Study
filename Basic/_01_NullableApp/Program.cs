d362ceac273d4ff9e53ceed7f4781a5434501805
taeho kim <hyssop9734@naver.com>
2020-01-05 00:20:29 +0900

    add basic

---
+++ Basic/_01_NullableApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_NullableApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? value = null;
+
+            if (value.HasValue)
+                Console.WriteLine($"value : {value.Value}");
+            else
+                Console.WriteLine($"value는 null");
+        }
+    }
+}
---
+++ Basic/_02_NullableStaticApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_NullableStaticApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? a = null;
+            int? b = null;
+            Console.WriteLine(Nullable.Equals<int>(a, b));
+
+            double? c = 10.11;
+            double? d = 10.110000;
+            Console.WriteLine(Nullable.Compare<double>(c, d));
+        }
+    }
+}
